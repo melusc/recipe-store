a94e160f153c10c8b824808de0c917164ffd2c7c
Luca Schnellmann <oss@lusc.ch>
2025-03-29 22:54:34 +0100

    feat: Terser plus icon using strokes

--- frontend/src/components/icons/plus.ts
+++ frontend/src/components/icons/plus.ts
@@ -23,9 +23,16 @@ import {$} from '../../$.js';
 export function iconPlus() {
 	return $`<svg
 		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 67.733 67.733"
+		viewBox="0 0 256 256"
 		style="vertical-align: top"
 	>
-  	<path d="M62.869 38.694H38.622v24.175a4.8643 4.8643 135 0 1-4.8643 4.8643 4.7923 4.7923 45.867 0 1-4.7192-4.8643V38.694H4.8635a4.8643 4.8643 45 0 1-4.8643-4.8643 4.8279 4.8279 135.43 0 1 4.8643-4.7913h24.175V4.8634a4.8643 4.8643 135 0 1 4.8643-4.8643 4.7923 4.7923 45.867 0 1 4.7192 4.8643v24.175h24.247a4.8643 4.8643 45 0 1 4.8643 4.8643 4.8279 4.8279 135.43 0 1-4.8643 4.7913z"/>
+  	<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="bevel"
+			stroke-width="24"
+			d="M12 128H244M128 12V244"
+		/>
 	</svg>`;
 }
