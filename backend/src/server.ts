476f33a3fdc920b01a478a5119010ae39104728b
Luca Schnellmann <oss@lusc.ch>
2025-03-30 17:26:38 +0200

    feat: remove cdnjs from content-security-policy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -45,11 +45,7 @@ export function setupServer(api: Api) {
 		helmet({
 			contentSecurityPolicy: {
 				directives: {
-					'script-src': [
-						"'self'",
-						'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/',
-						"'unsafe-inline'",
-					],
+					'script-src': ["'self'", "'unsafe-inline'"],
 					'img-src': [
 						"'self'",
 						'data:',
